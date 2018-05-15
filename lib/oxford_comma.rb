87c3e7f12e305ff695ac687b232769b0a100cce4
Robert Tabares <robtabares92@gmail.com>
2018-05-15 20:28:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,7 @@ def oxford_comma(array)
     return array.first
   elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]} and #{array[2]}"
   end
 end
