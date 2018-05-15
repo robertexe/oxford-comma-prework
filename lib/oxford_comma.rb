575c226498a8136b949c1c65b5342a62a6402b2d
Robert Tabares <robtabares92@gmail.com>
2018-05-15 20:48:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.first
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last_word = array.pop.to_s
+    last_word = ", and #{last_word}"
+    output = "#{array.join(', ')}#{last_word}"
+    return output
+  end
 end
